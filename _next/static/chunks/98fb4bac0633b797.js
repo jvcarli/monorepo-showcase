334ba7deebea21581e99adcbbbf5d328d74b3029
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2025-08-29 17:15:22 -0300

    chore: upload web app

---
+++ _next/static/chunks/98fb4bac0633b797.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_app", [
+  "static/chunks/e60ef129113f6e24.js",
+  "static/chunks/3c37f811e696a127.js",
+  "static/chunks/turbopack-c81133631fc9b327.js"
+])
---
+++ _next/static/chunks/c96e6abd2b05eee5.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/3c37f811e696a127.js",
+  "static/chunks/turbopack-2552f1dca6efb7fc.js"
+])
