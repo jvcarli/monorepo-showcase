8e1ec12950f8f87f062d6d52348f7817bd4f1fc8
Joao Vitor Carli Pereira <jvitor.carli@gmail.com>
2025-08-29 17:18:33 -0300

    fix: fix NextJS base path so it works with GitHub Pages

--- apps/web-client/next.config.ts
+++ apps/web-client/next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  /* config options here */
+  output: "export", // static export
+  basePath: "/monorepo-showcase", // GitHub Pages repo subpath
+  assetPrefix: "/monorepo-showcase", // fixes JS/CSS asset paths
+  trailingSlash: true, // ensures correct folder structure for GitHub Pages
 };
 
 export default nextConfig;
